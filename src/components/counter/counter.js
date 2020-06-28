eb45c1967326cd66de57fba28b8c331426b84114
Tommalieh <muhammedtommalieh@gmail.com>
2020-06-28 23:27:22 +0300

    Done

---
+++ src/components/counter/counter.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import './App.css';
+
+class Counter extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = { }
+    }
+    render(){
+        return(
+            <div>
+                <p></p>
+            </div>
+        )
+    }
+}
+
+export default Counter
